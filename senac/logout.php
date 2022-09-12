287ddbae2de49bbaae38583caed8e197ce4a5008
Vanessa Morais <101489085+Nessa-Morais@users.noreply.github.com>
2022-09-12 08:48:58 -0300

    Delete senac directory

--- senac/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION= array(); //carrega o array de dados
-session_destroy(); //destroi todas as sessões iniciadas
-header('location: \senac\index.php'); //redireciona para a área de login
-
-?>
